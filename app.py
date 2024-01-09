6cf17cc334c4949f2b3a70c913e470a9ef840d90
Hoon <a32252613@gmail.com>
2024-01-09 14:14:33 +0900

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask"
+    return render_template("res/page.html")
