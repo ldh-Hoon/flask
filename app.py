b2f0bb6684fb1c1ba6e155a45460daaed3ebade3
Hoon <a32252613@gmail.com>
2024-01-09 14:51:34 +0900

    .

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def hello():
     return render_template("page.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
